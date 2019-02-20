6fde4e0afa6f829d660d634076de9118f327865b
ramgopal18998 <30336016+ramgopal18998@users.noreply.github.com>
2019-02-20 14:29:29 +0530

    Update settings.py

--- Startup_incubator/Startup_incubator/settings.py
+++ Startup_incubator/Startup_incubator/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 EMAIL_USE_TLS=True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'test.webdevloper123@gmail.com'
-EMAIL_HOST_PASSWORD = 'goaebxtghuknhota'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
